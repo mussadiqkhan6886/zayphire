e4ab1fe1e2591db49d533688161d1481e08c2f78
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-16 13:44:25 +0500

    made laoding

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+'use client';
+
+import { motion, Variants } from "framer-motion";
 
 const Loader = () => {
-  return (
-    <div>
-      Loading....
-    </div>
-  )
-}
 
-export default Loader
+  return (
+    <div className="grid place-content-center bg-black h-screen px-4 py-24">
+      <BarLoader />
+    </div>
+  );
+};
+
+const variants = {
+  initial: {
+    scaleY: 0.5,
+    opacity: 0,
+  },
+  animate: {
+    scaleY: 1,
+    opacity: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 1,
+      ease: "circIn",
+    },
+  },
+} as Variants; 
+
+const BarLoader = () => {
+  return (
+    <motion.div
+      transition={{
+        staggerChildren: 0.25,
+      }}
+      initial="initial"
+      animate="animate"
+      className="flex gap-1"
+    >
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+    </motion.div>
+  );
+};
+
+export default Loader;
