0f4167bdb557aedc0be1dff05a3edade87bb05bd
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-16 12:53:24 +0500

    footer designed

--- components/userComponents/Footer.tsx
+++ components/userComponents/Footer.tsx
@@ -4,18 +4,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-black text-white flex justify-between'>
-      <div>
-        <ul>
-          <li><Link href="/faq">FAQ's</Link></li>
-          <li><Link href={"signup"}>Signup/Login</Link></li>
-          <li><Link href={"shipping-and-deliveries"}>Shipping & Deliveries</Link></li>
-          <li><Link href={"exchange-and-returns"}>Exchange & Returns</Link></li>
-        </ul>
-      </div>
-      <div>
-        <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={500} height={500} />
-        <p>&copy; Copyright Reserved by Zayphire 2025</p>
+    <footer className='bg-black text-white flex items-center justify-between'>
+      <div className="flex justify-between items-center ">
+        <div>
+          <ul>
+            <li><Link href="/faq">FAQ's</Link></li>
+            <li><Link href={"signup"}>Signup/Login</Link></li>
+            <li><Link href={"shipping-and-deliveries"}>Shipping & Deliveries</Link></li>
+            <li><Link href={"exchange-and-returns"}>Exchange & Returns</Link></li>
+          </ul>
+        </div>
+        <div>
+          <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={500} height={500} />
+          <p>&copy; Copyright Reserved by Zayphire 2025</p>
+        </div>
       </div>
       <div>
         <ul>
