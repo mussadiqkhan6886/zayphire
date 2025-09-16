b4d7865ceec48e12ffe74de33085f8f419a41f07
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-17 00:47:53 +0500

    created cat comp

--- components/userComponents/Categories.tsx
+++ components/userComponents/Categories.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Categories = () => {
+const Categories = ({cat}: {cat: string}) => {
   return (
     <div>
-      
+      <h3>{cat}</h3>
+      <h3>FRAGRANCES</h3>
+      <h3>SALE</h3>
+      <h3>T-SHIRTS & SHIRTS</h3>
+      <h3>FABRICS</h3>
     </div>
   )
 }
