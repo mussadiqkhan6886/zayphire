bac3d03b129c5b0184538454f65c2cb0611d5ed3
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-16 13:01:41 +0500

    footer deisgn choosing font

--- components/userComponents/Footer.tsx
+++ components/userComponents/Footer.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-black text-white flex items-center justify-between'>
-      <div className="flex justify-between items-center ">
+    <footer className='bg-black text-white flex items-center flex-col md:flex-row justify-between'>
+      <div className="flex justify-between items-center w-full lg:w-[60%] md:w-[70%] sm:flex-row flex-col">
         <div>
           <ul>
             <li><Link href="/faq">FAQ's</Link></li>
@@ -14,8 +14,8 @@ const Footer = () => {
             <li><Link href={"exchange-and-returns"}>Exchange & Returns</Link></li>
           </ul>
         </div>
-        <div>
-          <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={500} height={500} />
+        <div className='flex md:items-center flex-col items-end'>
+          <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={250} height={100} />
           <p>&copy; Copyright Reserved by Zayphire 2025</p>
         </div>
       </div>
