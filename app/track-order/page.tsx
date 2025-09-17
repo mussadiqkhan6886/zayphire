184856336079bd07f19a9bd0e2cf5b8db7db9d29
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-17 22:04:14 +0500

    side bar done

--- app/track-order/page.tsx
+++ app/track-order/page.tsx
@@ -1,10 +1,18 @@
+import Header from '@/components/userComponents/HeaderMain'
 import React from 'react'
 
 const Tracking = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Header />
+      <main className='pt-30 px-30'>
+        <h1 className='text-center text-3xl font-bold mb-5'>TRACK YOUR ORDER</h1>
+        <form className='flex gap-10 justify-center'>
+          <input type="text" placeholder='Order id' className='outline-none border border-black rounded-3xl w-[50%] bg-white text-black placeholder:text-gray-400 px-4 py-2' />
+          <button className='bg-black cursor-pointer text-white px-6 py-2 text-sm rounded-3xl'>TRACK</button>
+        </form>
+      </main>
+    </>
   )
 }
 
