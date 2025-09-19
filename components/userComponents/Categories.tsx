cc948f6163346fab0c6af07ff27a58c9ffda648b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-19 14:02:31 +0500

    fixed styling swiper

--- components/userComponents/Categories.tsx
+++ components/userComponents/Categories.tsx
@@ -9,7 +9,7 @@ const Categories = () => {
   
     return (
       <div className={`absolute z-50  bottom-10 left-0 px-10 md:px-0 md:left-[22%] text-white w-full md:w-[60%] ${view ? "hidden" : "block"} `}>
-        <div className='hidden sm:flex  justify-between items-center'>
+        <div className='hidden md:flex  justify-between items-center'>
             <Link href="#main">MEN</Link>
             <Link href={"#fragrances"}>FRAGRANCES</Link>
             <Link href={"#fabrics"}>FABRICS</Link>
