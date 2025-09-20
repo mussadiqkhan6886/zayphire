1359d49d148d8bc3379c158e45c4ce4261080f63
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-20 22:28:26 +0500

    fixed change

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,21 @@
 import HomeButton from "@/components/adminComponents/HomeButton";
+import { Metadata } from "next";
 import Link from "next/link";
 import { FaList, FaPlus, FaShoppingBag } from "react-icons/fa";
+
+export const metadata : Metadata = {
+    title: "Admin panel zayphire",
+    description: "Admin panel of zayphire protected data",
+    robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
+
+}
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
