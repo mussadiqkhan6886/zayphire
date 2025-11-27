ae06a319c5a3dd824a777a2972383b8d8fe896c4
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-28 01:05:44 +0500

    done

--- components/userComponents/Footer.tsx
+++ components/userComponents/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
   return (
     <footer className='md:h-screen flex  flex-col h-full'>
-      <div className='bg-white h-[20%] md:h-[60%] py-10 md:py-0 grid place-content-center'>
+      <div className='bg-white h-[40%] sm:h-[30%] md:h-[60%] py-10 md:py-0 grid place-content-center'>
         <div className='py-20 md:py-0'>
           <p className={`text-center border-b borer-black py-2 my-2 text-[25px] ${instrumentSerif.className}`}>Get the latest trends first</p>
           <div className='flex flex-wrap'>
@@ -24,22 +24,29 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className='bg-black px-7 text-white gap-5 md:gap-10 flex py-7 flex-col md:flex-row justify-between h-full md:h-[40%]'>
-        <div className="flex justify-between gap-10 items-center w-full lg:w-[60%] md:w-[70%] sm:flex-row flex-col-reverse">
-          <div>
+      <div className='bg-black px-7 text-white gap-5 md:gap-10 flex py-7 flex-col md:flex-row md:justify-between h-full md:h-[40%]'>
+        <div className="flex md:justify-between md:gap-10 items-center w-full lg:w-[60%] md:w-[70%] sm:flex-row flex-col-reverse text-sm">
+          <div className='flex items-center gap-5'>
             <ul className='leading-8'>
               <li><Link href="/details/#FAQ">FAQ&apos;s</Link></li>
               <li><Link href={"signup"}>Signup/Login</Link></li>
               <li><Link href={"/details/#shippingAndDeliveries"}>Shipping & Deliveries</Link></li>
               <li><Link href={"/details/#exchangeAndReturns"}>Exchange & Returns</Link></li>
             </ul>
+            <motion.div onViewportEnter={() => setView(true)} onViewportLeave={() => setView(false)} className='flex items-center justify-center text-left md:text-right text-sm md:hidden'>
+            <ul className='leading-8'>
+              <li><Link href={"/details/#about"}>About Us</Link></li>
+              <li><Link href={"/details/#contact"}>Contact Us</Link></li>
+              <li><Link href={"/details/#terms"}>Terms & Conditions</Link></li>
+            </ul>
+          </motion.div>
           </div>
-          <div className='flex md:items-center justify-between h-full flex-col items-end'>
-            <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={250} height={100} />
+          <div className='flex md:items-center justify-between h-full flex-col items-end scale-75 md:scale-100'>
+            <Image src="/zayphireGray.png" alt='zayphire footer logo image' width={250} height={100}  />
             <p className='text-sm'>&copy; Copyright Reserved by Zayphire 2025</p>
           </div>
         </div>
-          <motion.div onViewportEnter={() => setView(true)} onViewportLeave={() => setView(false)} className='flex items-center justify-center text-left md:text-right'>
+          <motion.div onViewportEnter={() => setView(true)} onViewportLeave={() => setView(false)} className='md:flex items-center justify-center text-left md:text-right text-sm hidden '>
             <ul className='leading-8'>
               <li><Link href={"/details/#about"}>About Us</Link></li>
               <li><Link href={"/details/#contact"}>Contact Us</Link></li>
