8f7e15573edfcd03ad740984fa66af5658731df6
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-01-04 21:03:36 +0500

    fixed whatsapp

--- components/userComponents/Whatsapp.tsx
+++ components/userComponents/Whatsapp.tsx
@@ -4,8 +4,8 @@ import { FaWhatsapp } from 'react-icons/fa'
 
 const Whatsapp = () => {
   return (
-    <div className='fixed bottom-5 right-5'>
-      <Link aria-label='whatsapp-link' href={"https://api.whatsapp.com/send/?phone=923265753305&text&type=phone_number&app_absent=0"}><FaWhatsapp aria-label='whatsapp button' size={36} /></Link>
+    <div className='fixed bg-green-500 rounded-full p-1.5  bottom-5 right-5 z-50'>
+      <Link className='text-white' aria-label='whatsapp-link' href={"https://api.whatsapp.com/send/?phone=923265753305&text&type=phone_number&app_absent=0"} target='_blank'><FaWhatsapp aria-label='whatsapp button' size={36} /></Link>
     </div>
   )
 }
